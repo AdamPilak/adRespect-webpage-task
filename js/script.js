79c04eded390317e7c4815b45f4c13633452e710
Unisuu <pilak.adam@gmail.com>
2023-08-22 20:07:45 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,40 +1,110 @@
 const masonry = Macy({
-    container: '.img-grid',
-    mobileFirst: true,
-    columns: 3,
-    margin: {
-        x: 10,
-        y: 10
-    },
-    breakAt: {
-        992: {
-            margin: {
-                x: 30,
-                y: 30
-            }
-        },
-        768: {
-            margin: {
-                x: 20,
-                y: 20
-            }
-        }
-    },
+	container: ".img-grid",
+	mobileFirst: true,
+	columns: 3,
+	margin: {
+		x: 10,
+		y: 10,
+	},
+	breakAt: {
+		992: {
+			margin: {
+				x: 30,
+				y: 30,
+			},
+		},
+		768: {
+			margin: {
+				x: 20,
+				y: 20,
+			},
+		},
+	},
 })
 
-const menuIcon = document.querySelector('#menu-icon');
-const nav = document.querySelector('.nav');
-const realizationsSection = document.querySelector('#realizations');
-const gradient = document.querySelector('.gradient');
-const expandCollapseBtn = document.querySelector('#expand-collapse');
+const sections = [...document.querySelectorAll('section')]
+const menuIcon = document.querySelector("#menu-icon")
+const nav = document.querySelector(".nav")
+const navLinks = document.querySelectorAll('header nav a')
+const subLinkBox = document.querySelector('.sub-link-box')
+const navLinkDropdownIcon = document.querySelector('.nav-link-dropdown-icon')
+const searchInput = document.querySelector(".search-box input")
+const searchIcon = document.querySelector(".search-icon")
+const realizationsSection = document.querySelector("#realizations")
+const gradient = document.querySelector(".gradient")
+const expandCollapseBtn = document.querySelector("#expand-collapse")
+const carouselContainer = document.querySelector(".carousel-container")
+const gridImages = [...document.querySelectorAll(".img-grid img")]
+const carouselImages = [...document.querySelectorAll(".carousel-inner img")]
 
-menuIcon.addEventListener('click', () => {
-    nav.classList.toggle('show');
-    menuIcon.classList.toggle('bx-x')
+
+//animations and scrollspy
+
+window.onscroll = () => {
+  sections.forEach((section) => {
+    const top = window.scrollY
+    const offset = section.offsetTop - 200
+    const height = section.offsetHeight
+    const id = section.getAttribute('id')
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove('active')
+        document
+          .querySelector(`header nav a[href*=${id}]`)
+          .classList.add('active')
+      })
+      section.classList.add('show-animate')
+    }
+		// two way animations
+		//  else {
+    //   section.classList.remove('show-animate')
+    // }
+  })
+}
+
+//nav logic
+
+navLinkDropdownIcon.addEventListener("click", () => {
+	subLinkBox.classList.toggle('show')
 })
 
-expandCollapseBtn.addEventListener('click', () => {
-    realizationsSection.classList.add('show');
-    gradient.classList.add('hide');
-    expandCollapseBtn.classList.add('hide');
+menuIcon.addEventListener("click", () => {
+	nav.classList.toggle("show")
+	menuIcon.classList.toggle("bx-x")
+})
+
+searchIcon.addEventListener("click", () => {
+	searchInput.classList.toggle("show")
+})
+
+//gallery expand/collapse logic
+
+expandCollapseBtn.addEventListener("click", () => {
+	realizationsSection.classList.add("show")
+	gradient.classList.add("hide")
+	expandCollapseBtn.classList.add("hide")
+})
+
+//carousel logic
+
+gridImages.forEach(gridImage => {
+	gridImage.addEventListener("click", () => {
+		carouselImages.forEach(carouselImage => {
+			if (carouselImage.attributes.src.nodeValue === gridImage.attributes.src.nodeValue) {
+				carouselImage.parentElement.classList.add("active")
+			}
+		})
+		carouselContainer.classList.add("show")
+	})
+})
+
+window.addEventListener("keydown", e => {
+	if (e.key === "Escape") {
+		if (carouselContainer.classList.contains("show")) {
+			carouselContainer.classList.remove("show")
+			carouselImages.forEach(carouselImage => {
+				carouselImage.parentElement.classList.remove("active")
+			})
+		}
+	}
 })
